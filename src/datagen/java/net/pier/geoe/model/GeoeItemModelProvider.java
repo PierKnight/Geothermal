3c8bb1a1c814af9f9a26eace8da0f9efdc5127a7
Pierknight <45203064+PierKnight@users.noreply.github.com>
2024-07-31 15:07:58 +0200

    Item Model Provider

--- src/datagen/java/net/pier/geoe/model/GeoeItemModelProvider.java
+++ src/datagen/java/net/pier/geoe/model/GeoeItemModelProvider.java
@@ -1,16 +1,12 @@
 package net.pier.geoe.model;
 
-import net.minecraft.client.renderer.block.model.ItemModelGenerator;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.HashCache;
-import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.pier.geoe.Geothermal;
 import net.pier.geoe.register.GeoeBlocks;
 
-import java.io.IOException;
 import java.util.Objects;
 
 public class GeoeItemModelProvider extends ItemModelProvider {
