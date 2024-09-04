faeef9f0e1cb854d98d086c3ec469841c17e8e7b
Pierknight <45203064+PierKnight@users.noreply.github.com>
2024-09-04 19:20:42 +0200

    Added Reservoir Noise Sampling Added Reservoir Noise Maps Maps are traded from a new type of villager, the geologist

--- src/main/java/net/pier/geoe/register/GeoeItems.java
+++ src/main/java/net/pier/geoe/register/GeoeItems.java
@@ -5,6 +5,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.pier.geoe.Geothermal;
+import net.pier.geoe.item.ReservoirMap;
 import net.pier.geoe.item.WrenchItem;
 
 public class GeoeItems
@@ -13,6 +14,7 @@ public class GeoeItems
     public static final DeferredRegister<Item> REGISTER = DeferredRegister.create(ForgeRegistries.ITEMS, Geothermal.MODID);
 
     public static final RegistryObject<Item> WRENCH = REGISTER.register("wrench", WrenchItem::new);
+    public static final RegistryObject<Item> RESERVOIR_MAP = REGISTER.register("reservoir_map", ReservoirMap::new);
 
 
 }
